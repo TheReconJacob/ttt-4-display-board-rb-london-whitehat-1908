dccdd63582367f792862473bf7f8a4d08934997b
Jacob Vibert <jvpokeyjake@gmail.com>
2019-08-06 14:05:33 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 board = [" x "," x "," x "," x "," x "," x "," x "," x "," x "]
 
-def display_board
+def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[3]} "
   puts "-----------"
   puts " #{board[4]} | #{board[5]} | #{board[6]} "
